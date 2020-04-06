7f340e8b6c604ab7f19c94b66ad33f8433a22db2
Dustin Pianalto <dustin@djpianalto.com>
2020-04-06 11:48:35 -0800

    Update comments for Godoc

--- context.go
+++ context.go
@@ -16,12 +16,12 @@ func (c *Context) Send(message string) (*discordgo.Message, error) {
 	return c.Session.ChannelMessageSend(c.Channel.ID, message)
 }
 
-// Send an embed to originating channel
+// SendEmbed sends an embed to originating channel
 func (c *Context) SendEmbed(embed *discordgo.MessageEmbed) (*discordgo.Message, error) {
 	return c.Session.ChannelMessageSendEmbed(c.Channel.ID, embed)
 }
 
-// Send a file to originating channel
+// SendFile sends a file to originating channel
 func (c *Context) SendFile(filename string, file io.Reader) (*discordgo.Message, error) {
 	return c.Session.ChannelFileSend(c.Channel.ID, filename, file)
 }
