28adfdeb0979df64378a15d9ddee43132360c23b
Dustin Pianalto <dustin@djpianalto.com>
2020-04-05 17:21:25 -0800

    Remove helper functions

--- djpianalto.com/disgoman/disgoman.go
+++ djpianalto.com/disgoman/disgoman.go
@@ -8,30 +8,11 @@ package disgoman
  * Disgoman (c) 2020 Dusty.P/dustinpianalto
  */
 
-func GetCommandManager(prefixes PrefixesFunc, owners []string, ignoreBots, checkPerms bool) CommandManager {
-	return CommandManager{
-		Prefixes:         prefixes,
-		Owners:           owners,
-		StatusManager:    GetDefaultStatusManager(),
-		Commands:         make(map[string]*Command),
-		Aliases:          make(map[string]string),
-		IgnoreBots:       ignoreBots,
-		CheckPermissions: checkPerms,
-	}
-}
-
-func GetStatusManager(values []string, interval string) StatusManager {
-	return StatusManager{
-		Values:   values,
-		Interval: interval,
-	}
-}
-
 func GetDefaultStatusManager() StatusManager {
-	return GetStatusManager(
+	return StatusManager{
 		[]string{
 			"Golang!",
 			"DiscordGo!",
 			"Disgoman!",
-		}, "10s")
+		}, "10s"}
 }
